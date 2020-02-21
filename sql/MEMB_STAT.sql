5d7b1f1d0ef649f745d427970848ca35af3637c1
Emil Nikolov <emil@Emils-MacBook-Pro.local>
2020-02-21 19:54:59 +0100

    sql files changes

--- sql/MEMB_STAT.sql
+++ sql/MEMB_STAT.sql
@@ -1,15 +1,6 @@
 USE [MuOnline]
 GO
 
-UPDATE [dbo].[MEMB_STAT]
-   SET [memb___id] = <memb___id, varchar(10),>
-      ,[ConnectStat] = <ConnectStat, tinyint,>
-      ,[ServerName] = <ServerName, varchar(10),>
-      ,[IP] = <IP, varchar(15),>
-      ,[ConnectTM] = <ConnectTM, smalldatetime,>
-      ,[DisConnectTM] = <DisConnectTM, smalldatetime,>
-      ,[TotalTime] = <TotalTime, int,>
- WHERE <Search Conditions,,>
+ALTER TABLE [dbo].[MEMB_STAT] ADD [TotalTime] INT;
+
 GO
-
-
--- sql/_nyxResources.sql
+++ sql/_nyxResources.sql
@@ -1,3 +1,5 @@
+
+
 USE [MuOnline]
 GO
 
